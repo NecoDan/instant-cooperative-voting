50889ac4161faa386841d7d607ff4eec115eb874
Daniel Santos <neco.daniel@gmail.com>
2021-11-11 21:47:43 -0300

    refatoracao retorno excecao enum tipo permissao voto

--- src/main/java/br/com/ntconsultant/instant/cooperative/voting/enums/TypePermittedCpfVote.java
+++ src/main/java/br/com/ntconsultant/instant/cooperative/voting/enums/TypePermittedCpfVote.java
@@ -1,5 +1,7 @@
 package br.com.ntconsultant.instant.cooperative.voting.enums;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,6 +11,7 @@ import java.util.stream.Stream;
 /**
  * @author Daniel Santos
  */
+@Slf4j
 public enum TypePermittedCpfVote {
 
     ABLE_TO_VOTE("ABLE_TO_VOTE"),
@@ -38,11 +41,19 @@ public enum TypePermittedCpfVote {
     public static TypePermittedCpfVote fromCode(String code) {
         if (lookup.containsKey(code))
             return lookup.get(code);
-        throw new IllegalArgumentException("Invalid Type Permitted Cpf Vote Code: " + code);
+        throw getIllegalArgumentException(code);
     }
 
-    public static TypePermittedCpfVote of(String codigo) {
-        return Stream.of(TypePermittedCpfVote.values()).filter(p -> p.getCode().equals(codigo)).findFirst().orElseThrow(IllegalArgumentException::new);
+    public static TypePermittedCpfVote of(String code) {
+        return Stream.of(TypePermittedCpfVote.values())
+                .filter(p -> p.getCode().equals(code))
+                .findFirst()
+                .orElseThrow(() -> getIllegalArgumentException(code));
+    }
+
+    private static IllegalArgumentException getIllegalArgumentException(String code) {
+        log.error("[instant-cooperative-voting] [type permitted cpf vote] Invalid Type Permitted Cpf Vote Code {} ", code);
+        return new IllegalArgumentException("Invalid Type Permitted Cpf Vote Code: " + code);
     }
 
     public String getCode() {
